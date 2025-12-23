0c954b99e75373627ac2804829f29c1072522f4b
mohamedshamlola412-cmd <mohamedshamlola412@gmail.com>
2025-12-24 00:53:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,4 @@ def predict():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)ray(data['features']).reshape(1, -1)
-        prediction = model.predict(features)
-        return jsonify({'prediction': int(prediction[0])})
-    except Exception as e:
-        return jsonify({'error': str(e)})
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
